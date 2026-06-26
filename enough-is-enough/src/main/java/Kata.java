b8893223df2f12e4fa5ca9f68912692747d0efaa
EdwinBoatswainHO <edwin.boatswain@homeoffice.gov.uk>
2026-06-26 18:17:25 +0100

    Got lazy and started adding katas to the same project

--- enough-is-enough/src/main/java/Kata.java
+++ enough-is-enough/src/main/java/Kata.java
@@ -50,4 +50,63 @@ public class Kata {
 
         return matched;
     }
+
+    public static int findShort(String s) {
+        return Arrays.stream(s.split("\\s+"))
+                .mapToInt(String::length)
+                .reduce(Integer.MAX_VALUE, Math::min);
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++)
+                if ((numbers[i] + numbers[j]) == target) {
+                    return new int[]{i,j};
+                }
+        }
+        return null;
+    }
+    public static String rot13(String str) {
+        final String lookupLower = "nopqrstuvwxyzabcdefghijklm";
+        final String lookupUpper = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+        StringBuilder result = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+
+            if (Character.isAlphabetic(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    result.append(lookupLower.charAt(ch - 'a'));
+                } else {
+                    result.append(lookupUpper.charAt(ch - 'A'));
+                }
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    // find outlier
+    static int find(int[] integers) {
+        boolean evens = mostlyEven(integers);
+        for (int current : integers) {
+            if (evens && (current % 2) != 0)
+                return current;
+            else if (!evens && (current % 2) == 0)
+                return current;
+        }
+        return 0;
+    }
+
+    static boolean mostlyEven(int[] integers) {
+        int even = 0;
+        int odd  = 0;
+        for (int i = 0; i < 3; i++) {
+            if ((integers[i] % 2) == 0 )
+               even ++;
+            else
+                odd ++;
+        }
+
+        return even > odd;
+    }
 }
--- enough-is-enough/src/test/java/KataTest.java
+++ enough-is-enough/src/test/java/KataTest.java
@@ -1,6 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class KataTest {
 
@@ -60,5 +67,51 @@ class KataTest {
         testing(Kata.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 3), "ixoyx3452zzzzzzzzzzzz");
         testing(Kata.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 15), "");
         testing(Kata.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 0), "");
+    }
+
+    // find shortest words
+    @Test
+    void findShort() {
+        assertEquals(3, Kata.findShort("bitcoin take over the world maybe who knows perhaps"));
+        assertEquals(3, Kata.findShort("turns out random test cases are easier than writing out basic ones"));
+        assertEquals(2, Kata.findShort("Let's travel abroad shall we"));
+    }
+
+    // Two sum test
+    static Stream<Arguments> basicTests() {
+        return Stream.of(
+                arguments(new int[]{1,2,3},          4,     new int[]{0,2}),
+                arguments(new int[]{1234,5678,9012}, 14690, new int[]{1,2}),
+                arguments(new int[]{2,2,3},          4,     new int[]{0,1}),
+                arguments(new int[]{2,3,1},          4,     new int[]{1,2})
+        );
+    }
+    @ParameterizedTest(name="numbers: {0}, target: {1}, expected: {2}")
+    @MethodSource
+    @DisplayName("Basic tests")
+    void basicTests(int[] numbers, int target, int[] expected) {
+        int[] actual = Kata.twoSum(numbers.clone(), target);
+        assertNotNull(actual, "Should return an array");
+        assertEquals(2, actual.length, "Returned array must be of length 2");
+        assertNotEquals(actual[0], actual[1], "Indices must be distinct");
+        int num1 = numbers[actual[0]];
+        int num2 = numbers[actual[1]];
+        assertEquals(target, num1 + num2, String.format("Numbers %d, %d at positions %d, %d do not add up to target", num1, num2, actual[0], actual[1]));
+    }
+
+    // rot13
+    @Test
+    void testRot13() {
+        // assertEquals("expected", "actual");
+        assertEquals("grfg", Kata.rot13("test"), "Input: \"test\"");
+        assertEquals("Grfg", Kata.rot13("Test"), "Input: \"Test\"");
+    }
+
+    // find outlier
+    @Test
+    void findOutliers() {
+        assertEquals(3, Kata.find(new int[] {2, 6, 8, -10, 3}));
+        assertEquals(206847684, Kata.find(new int[] {206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781}));
+        assertEquals(0, Kata.find(new int[] {Integer.MAX_VALUE, 0, 1}));
     }
 }
